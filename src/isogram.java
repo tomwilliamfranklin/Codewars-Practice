b4fc16fc3428dd9b4521bc2425374ab2c41bd01c
Tom <tomwilliamfranklin@gmail.com>
2019-06-01 23:00:09 +0100

    Isogram
    " An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. An empty string counts as a isogram. "

--- src/isogram.java
+++ src/isogram.java
@@ -1,12 +1,24 @@
 public class isogram {
+    /*
+        An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+        Implement a function that determines whether a string that contains only letters is an isogram.
+        the empty string is an isogram. Ignore letter case.
+     */
     public static boolean  isIsogram(String str) {
         String word = str.toLowerCase();
-        for(char letter : word.toCharArray()) {
-            {
-
+        for (char letter : word.toCharArray()) {
+            int count = 0;
+            for(int i=0; i<word.length(); i++) {
+                if (word.charAt(i) == letter) {
+                    count++;
+                }
+            }
+            if(count >= 2) {
+                return false;
             }
         }
-
-        // ...
-    } 
+    return true;
+    }
 }
+
+
--- src/main.java
+++ src/main.java
@@ -5,8 +5,10 @@ class main
 
     public static void main(String args[])
     {
-        //System.out.println(XO.getXO("xxx23424esdsfvxXXOOooo"));
+        System.out.println(XO.getXO("xxx23424esdsfvxXXOOooo"));
         System.out.println(Vowels.getCount("aeiou"));
+        System.out.println(isogram.isIsogram("aaddduiiic"));
+        System.out.println(isogram.isIsogram("afghtnbi"));
 
     }
 }
